c77d5761af63315b8973161d59191248d50fc9f4
Polkovn1k <wa0s@narod.ru>
2019-01-18 12:30:58 +0300

    index--9

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*var header = document.querySelector(".header");
+var mainVal = document.querySelector(".main");
+
+var heightVal = header.offsetHeight + "px";
+mainVal.style.marginTop = heightVal;
+
+window.addEventListener("resize", function() {
+  heightVal = header.offsetHeight + "px";
+  mainVal.style.marginTop = heightVal;
+});*/
+
+
+
+
+var header = document.querySelector(".header");
+var mainVal = document.querySelector(".main");
+
+var heightVal = header.offsetHeight + "px";
+mainVal.style.marginTop = heightVal;
+
+window.addEventListener("resize", function() {
+  heightVal = header.offsetHeight + "px";
+  mainVal.style.marginTop = heightVal;
+});
