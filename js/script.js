b233715df0ebece788173d7e196592a811ee89ef
Polkovn1k <wa0s@narod.ru>
2019-01-18 15:34:53 +0300

    index--10

--- js/script.js
+++ js/script.js
@@ -22,3 +22,22 @@ window.addEventListener("resize", function() {
   heightVal = header.offsetHeight + "px";
   mainVal.style.marginTop = heightVal;
 });
+
+
+
+var header = document.querySelector(".header");
+var headerNav = header.querySelector(".navigation--header");
+var navTog = header.querySelector(".header__nav-tog");
+var navTogOpen = header.querySelector(".header__nav-tog .appear");
+var navTogClose = header.querySelector(".header__nav-tog: .hidden");
+
+navTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  headerNav.classList.toggle("navigation--header-open");
+});
+
+navTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  navTogOpen.classList.toggle("hidden");
+  navTogClose.classList.toggle("hidden");
+});
