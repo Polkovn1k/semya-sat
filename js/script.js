b9c00e2753c1fc89ecd1cf9f2c0a7422809ade9e
Polkovn1k <wa0s@narod.ru>
2019-01-18 17:10:15 +0300

    index--11

--- js/script.js
+++ js/script.js
@@ -11,9 +11,64 @@ window.addEventListener("resize", function() {
 
 
 
+//показываем навигацию по клику + меняем иконку на тоглере навигации
+var header = document.querySelector(".header");
+var headerNav = header.querySelector(".navigation--header");
+var navTog = header.querySelector(".header__nav-tog");
+var navTogSvg = header.querySelectorAll(".header__nav-tog svg");
 
+navTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  headerNav.classList.toggle("visually-hidden");
+});
+
+navTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (var i = 0; i < navTogSvg.length; i++) {
+    navTogSvg[i].classList.toggle("visually-hidden");
+  };
+});
+
+
+
+
+
+
+//показываем поиск по клику + меняем иконку на тоглере поиска
+var header = document.querySelector(".header");
+var headerSearchItems = header.querySelectorAll(".header__panel > *:not(.header__tog-search");
+var searchTog = header.querySelector(".header__tog-search");
+var searchTogSvg = header.querySelectorAll(".header__tog-search svg");
+
+searchTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (var i = 0; i < headerSearchItems.length; i++) {
+    headerSearchItems[i].classList.toggle("visually-hidden");
+  };
+});
+
+searchTog.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (var i = 0; i < searchTogSvg.length; i++) {
+    searchTogSvg[i].classList.toggle("visually-hidden");
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+//при событиях клика или изменении вьюпорта пересчитывается высота блока и ставится отступ у main
 var header = document.querySelector(".header");
 var mainVal = document.querySelector(".main");
+var navTog = header.querySelector(".header__nav-tog");
+var searchTog = header.querySelector(".header__tog-search");
 
 var heightVal = header.offsetHeight + "px";
 mainVal.style.marginTop = heightVal;
@@ -23,21 +78,12 @@ window.addEventListener("resize", function() {
   mainVal.style.marginTop = heightVal;
 });
 
-
-
-var header = document.querySelector(".header");
-var headerNav = header.querySelector(".navigation--header");
-var navTog = header.querySelector(".header__nav-tog");
-var navTogOpen = header.querySelector(".header__nav-tog .appear");
-var navTogClose = header.querySelector(".header__nav-tog: .hidden");
-
-navTog.addEventListener("click", function(event) {
-  event.preventDefault();
-  headerNav.classList.toggle("navigation--header-open");
+navTog.addEventListener("click", function() {
+  heightVal = header.offsetHeight + "px";
+  mainVal.style.marginTop = heightVal;
 });
 
-navTog.addEventListener("click", function(event) {
-  event.preventDefault();
-  navTogOpen.classList.toggle("hidden");
-  navTogClose.classList.toggle("hidden");
+searchTog.addEventListener("click", function() {
+  heightVal = header.offsetHeight + "px";
+  mainVal.style.marginTop = heightVal;
 });
